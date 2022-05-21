25727c4d783a1843268346d31e6fe370a3bb2cee
khwiri <5530052+khwiri@users.noreply.github.com>
2022-05-21 19:39:20 -0400

    :sparkles: Add Support for Decoding Tokens from Standard Input

--- jwt_debugger/command.py
+++ jwt_debugger/command.py
@@ -8,6 +8,9 @@ from click import File
 from click import option
 from click import command
 from click import argument
+from click import get_text_stream
+from click.core import Context
+from click.core import Argument
 from click.exceptions import UsageError
 
 from jwt_debugger.decoder import decode_token
@@ -16,10 +19,17 @@ from jwt_debugger.decoder import load_jwkset_from_oidc_url
 from jwt_debugger.decoder import resolve_jwks_uri_from_oidc_provider
 
 
+def read_token_argument(unused_context :Context, unused_argument :Argument, value :Optional[str]) -> str:
+    if value is None:
+        stdin_stream = get_text_stream('stdin')
+        return stdin_stream.read().strip()
+    return value
+
+
 @command()
-@option('--public-key', type=File(), help='JSON Web Key in JSON or PEM format for signature verification.')
-@option('--oidc-provider-url', help='OpenID Connect Provider URL where JSON Web Key Set can be pulled for signature verification.')
-@argument('token', required=True)
+@option('--public-key', type=File(), help='JSON Web Key in JSON or PEM format for signature verification')
+@option('--oidc-provider-url', help='OpenID Connect Provider URL where JSON Web Key Set can be pulled for signature verification')
+@argument('token', required=False, callback=read_token_argument)
 def cli(token :str, public_key :Optional[TextIOWrapper] =None, oidc_provider_url :str =None) -> None:
     if all([public_key, oidc_provider_url]):
         raise UsageError('The following options can not be used together (--public-key, --oidc-provider-url).')
