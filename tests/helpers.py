a25fcc46577ea9292ec98265e1deeb92583d90ef
khwiri <5530052+khwiri@users.noreply.github.com>
2022-06-01 20:25:28 -0400

    :white_check_mark: Refactor Tests
    - Move Keys and Tokens to Example Files
    - Add Helpers for Loading Example Files
    - Cleanup Token Decoder Tests
    - Split and Cleanup OIDC Provider JWKS Resolver Tests

---
+++ tests/helpers.py
@@ -0,0 +1,54 @@
+import json
+from typing import Dict
+from typing import Union
+from typing import Optional
+from pathlib import Path
+
+from jwcrypto.jwk import JWK
+
+from jwt_debugger.decoder import DecodedToken
+
+
+class UnsupportedPublicKeyFormat(Exception):
+    pass
+
+
+def load_public_key(example :Union[int, str], key_format :str =None) -> JWK:
+    key_format = 'json' if key_format is None else key_format
+    if key_format not in ('json', 'pem'):
+        raise UnsupportedPublicKeyFormat()
+
+    example_file_name   = f'public_key-example_{example}.{key_format}'
+    example_key_path    = Path('.') / 'tests' / 'examples' / example_file_name
+    example_key_content = example_key_path.read_text()
+
+    if key_format == 'json':
+        return JWK(**json.loads(example_key_content))
+
+    if key_format == 'pem':
+        return JWK.from_pem(example_key_content)
+
+
+def load_encoded_token(example :Union[int, str]) -> str:
+    example_file_name  = f'encoded_token-example_{example}.txt'
+    example_token_path = Path('.') / 'tests' / 'examples' / example_file_name
+
+    return example_token_path.read_text().strip() # stripping trailing newline characters
+
+
+def load_decoded_token_as_json(example: Union[int, str]) -> Dict:
+    example_file_name  = f'decoded_token-example_{example}.json'
+    example_token_path = Path('.') / 'tests' / 'examples' / example_file_name
+
+    with example_token_path.open() as f:
+        return json.load(f)
+
+
+def load_decoded_token(example :Union[int, str], verified :Optional[bool] =None) -> DecodedToken:
+    token_as_json = load_decoded_token_as_json(example)
+    return DecodedToken(
+        token    = 'faux-token',
+        header   = token_as_json.get('header',  {}),
+        payload  = token_as_json.get('payload', {}),
+        verified = verified
+    )
---
+++ tests/test_jwks_providers.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+from unittest.mock import Mock
+from unittest.mock import patch
+
+from parameterized import parameterized
+
+from jwt_debugger.decoder import resolve_jwks_uri_from_open_id_connect_provider
+
+
+class TestOIDCProvider(TestCase):
+    def setUp(self):
+        self._faux_jwks_uri = 'faux-uri'
+        self._mock_configuration_response = Mock()
+        self._mock_configuration_response.json.return_value = {'jwks_uri': self._faux_jwks_uri}
+
+    @parameterized.expand([
+        (
+            'https://example.com',
+            'https://example.com/.well-known/openid-configuration',
+        ),
+        (
+            'https://example.com/issuer1',
+            'https://example.com/issuer1/.well-known/openid-configuration',
+        ),
+    ])
+    def test_jwks_from_provider_url(self, provider_url :str, configuration_url :str):
+        with patch('requests.get', return_value=self._mock_configuration_response) as mock_requests_get:
+            jwks_uri = resolve_jwks_uri_from_open_id_connect_provider(provider_url)
+            mock_requests_get.assert_called_with(configuration_url)
+            self.assertEqual(jwks_uri, self._faux_jwks_uri)
+
+    def test_configuration_does_not_include_jwks_uri(self):
+        provider_url  = 'https://example.com/.well-known/openid-configuration'
+        mock_response = Mock()
+        mock_response.json.return_value = {}
+        with patch('requests.get', return_value=mock_response), self.assertRaises(KeyError) as raise_context:
+            resolve_jwks_uri_from_open_id_connect_provider(provider_url)
+
+        self.assertEqual(
+            str(raise_context.exception),
+            f"'OpenID Connect Configuration({provider_url}) does not contain jwks_uri endpoint.'"
+        )
+
+    def test_identity_server_jwks_uri(self):
+        jwks_uri = 'https://demo.identityserver.io/.well-known/openid-configuration/jwks'
+        with patch('requests.get', return_value=self._mock_configuration_response) as mock_requests_get:
+            jwks_uri_resolved = resolve_jwks_uri_from_open_id_connect_provider(jwks_uri)
+            mock_requests_get.assert_not_called()
+            self.assertEqual(jwks_uri_resolved, jwks_uri)
